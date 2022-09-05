0f618932eecf74129671c7482b229f6f8e8abdf6
appsdevadmin <appsdev_admin@nnpcgroup.com>
2022-09-05 14:54:30 +0100

    Updated users table name

--- app/users.php
+++ app/users.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class users extends Model {
 
+    protected $table = 'users_table';
+
 	protected $fillable=['id_no','access','role','name','email'];
 
 }
--- database/migrations/2022_09_01_094502_create_users_table.php
+++ database/migrations/2022_09_01_094502_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration {
+class CreateUsers_tableTable extends Migration {
 
 	/**
 	 * Run the migrations.
@@ -13,7 +13,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function up()
 	{
-		Schema::create('users', function(Blueprint $table)
+		Schema::create('users_table', function(Blueprint $table)
 		{
 			$table->increments('id');
 			$table->string('id_no');
@@ -33,7 +33,7 @@ class CreateUsersTable extends Migration {
 	 */
 	public function down()
 	{
-		Schema::drop('users');
+		Schema::drop('users_table');
 	}
 
 }
