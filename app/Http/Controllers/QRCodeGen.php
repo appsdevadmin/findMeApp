a8df98902458e66f877a0a4243eb7a1f9f4e8b85
Abubakar S. Bashir <21093@nnpcgroup.com>
2022-10-25 03:59:43 -0700

    Generate QR for 20988

--- app/Http/Controllers/QRCodeGen.php
+++ app/Http/Controllers/QRCodeGen.php
@@ -14,15 +14,16 @@ class QRCodeGen extends Controller
 	public function generateQR()
     {		
 			$logopath = '\public\img\x11.png';
-			$all_staffx = staff_data::wherebetween('id', [6301,6873])->get();
+			$all_staffx = staff_data::where('staff_id',20988)->get();
+			
 				ini_set('max_execution_time', 0);
 				foreach($all_staffx as $asx) {
 					$qr_data = 'https://findme.nnpcgroup.com/'.$asx->unique_id;
 					QrCode::size(300)->format('png')->style('square')->margin(3)
 					->eye('square')->errorCorrection('H')->merge($logopath, .2)
-					->generate($qr_data, public_path('QRCode/'.$asx->staff_id.'.png'));
+					->generate($qr_data, public_path('img/'.$asx->staff_id.'.png'));
 				}
-			return view('qrcode' ,compact('all_staffx'));
+			return ;
     }
 	
 }
